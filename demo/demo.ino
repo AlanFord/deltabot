ec24e21059f8cbca97a2925c3db98683ed7699b4
Alan Ford <alanford354@gmail.com>
2018-03-25 20:48:57 -0400

    formatting

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,7 +1,7 @@
 /*
- * Caution: the code calculates servo angles, likely from -90 to +90, where 0 is at the horizontal.
- *  -->>>   how does the bot configure the servos??
- */
+   Caution: the code calculates servo angles, likely from -90 to +90, where 0 is at the horizontal.
+    -->>>   how does the bot configure the servos??
+*/
 
 #include <Servo.h>
 
@@ -13,18 +13,18 @@ double baseLength = 5;      // distnace from center of base to pivot point of th
 double phi[] = {0, 120, 240}; // rotational location of each leg, in degrees.
 
 Servo myServos[3];
-int servoPins[] = {8,9,10};
+int servoPins[] = {8, 9, 10};
 
 
 
 
 void setup() {
   // convert phi to radians, simplifying the math later on
-  for (int i=0; i < 3; i++) {
-    phi[i] = phi[i]/180. * PI;
+  for (int i = 0; i < 3; i++) {
+    phi[i] = phi[i] / 180. * PI;
   }
   // set up the servo motors
-  for (int i=0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {
     myServos[i].attach(servoPins[i]);
   }
 }
@@ -34,44 +34,43 @@ void loop() {
   // note that "xPrime" is the distance to the right
   //           "zPrime" is the distance to the front
   //           "yPrime" is the distance down below the base (should be negative, but it doesn't really matter)
-  double xPrime[] = {0,0,0,0};
-  double yPrime[] = {0,0,0,0};
-  double zPrime[] = {0,0,0,0};
+  double xPrime[] = {0, 0, 0, 0};
+  double yPrime[] = {0, 0, 0, 0};
+  double zPrime[] = {0, 0, 0, 0};
 
-  
-  int servoAngle[] = {0,0,0};
+
+  int servoAngle[] = {0, 0, 0};
 
   // loop through all of the required gripper positions, pausing between
   for (int j = 0; j < 4; j++) {
     // calculate angle of each of three servos for the current gripper position
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++) {
       double x = xPrime[j] * cos(phi[i]) - zPrime[j] * sin(phi[i]);
       double y = yPrime[j];
       double z = xPrime[j] * sin(phi[i]) + zPrime[j] * cos(phi[i]);
-      double c = sqrt(pow(x + effectorLength - baseLength,2) + y*y);
-      double a = sqrt(shinLength*shinLength - z*z);
-      double alpha = acos( (-(a*a)+thighLength*thighLength+c*c) / (2*thighLength*c) );
+      double c = sqrt(pow(x + effectorLength - baseLength, 2) + y * y);
+      double a = sqrt(shinLength * shinLength - z * z);
+      double alpha = acos( (-(a * a) + thighLength * thighLength + c * c) / (2 * thighLength * c) );
       double beta = atan2(y, x + effectorLength - baseLength);
-      servoAngle[i] = round((alpha - abs(beta))/PI*180);  // servo angles are in degrees, and are integers
+      servoAngle[i] = round((alpha - abs(beta)) / PI * 180); // servo angles are in degrees, and are integers
     }
     // sanity check of the servo angles
     bool bogus = false;
-    for (int i=0; i<3; i++) {
-       if (servoAngle[i]<0 or servoAngle[i]>180) {
-          bogus = true;
-       }
+    for (int i = 0; i < 3; i++) {
+      if (servoAngle[i]<0 or servoAngle[i]>180) {
+        bogus = true;
+      }
     }
     // set the servos to the appropriate angles
     // keep this loop separate from the others to ensure all servos are set is rapid order
     // (keep the legs from going whacko!)
     if (bogus == false) {
-      for (int i=0; i<3; i++) {
-          myServos[i].write(servoAngle[i]);
+      for (int i = 0; i < 3; i++) {
+        myServos[i].write(servoAngle[i]);
       }
     }
     delay(2000);
   }
-
 }
 
- 
+
